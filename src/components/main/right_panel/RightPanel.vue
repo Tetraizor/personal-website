c33668bac3943dfd4fadcdb75d81378bb0cd50e8
Umuthan Özel <ozelumuthan@gmail.com>
2024-02-28 15:32:57 +0300

    Add right panel scrolling pattern

--- src/components/main/right_panel/RightPanel.vue
+++ src/components/main/right_panel/RightPanel.vue
@@ -27,8 +27,30 @@ export default {
   width: 418px;
   background-color: $background-secondary;
 
+  background-image: url('../../../assets/patterns/pattern.svg');
+  background-repeat: repeat;
+
+  background-size: 750px 866px;
+  background-position: 0 0;
+
   display: flex;
   flex-direction: row;
+
+  animation: slide 100s linear infinite;
+
+  @keyframes slide {
+    0% {
+      background-position: 0 0;
+    }
+
+    50% {
+      background-position: 750px 433px;
+    }
+
+    100% {
+      background-position: 1500px 866px;
+    }
+  }
 
   .divider {
     width: 2px;
